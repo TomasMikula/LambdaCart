d36f0635eeb7361c88ce19a5bb4327b7319fbc8e
Tomas Mikula <tomas.mikula@gmail.com>
2017-09-08 17:02:41 +0200

    Make ANone a singleton.

--- src/main/scala/lambdacart/util/typealigned/AOption.scala
+++ src/main/scala/lambdacart/util/typealigned/AOption.scala
@@ -8,13 +8,24 @@ import scalaz.Leibniz.===
   */
 sealed abstract class AOption[F[_, _], A, B]
 
+case class ASome[F[_, _], A, B](value: F[A, B]) extends AOption[F, A, B]
+
 /**
   * The empty case contains evidence of type equality
   * to overcome the limitations of pattern-matching on GADTs.
   */
-case class ANone[F[_, _], A, B](ev: A === B) extends AOption[F, A, B]
-case class ASome[F[_, _], A, B](value: F[A, B]) extends AOption[F, A, B]
+sealed abstract case class ANone[F[_, _], A, B]() extends AOption[F, A, B] {
+  def   subst[G[_]](ga: G[A]): G[B]
+  def unsubst[G[_]](gb: G[B]): G[A]
+  def leibniz: A === B = subst[A === ?](Leibniz.refl)
+}
 
 object AOption {
-  def empty[F[_, _], A]: AOption[F, A, A] = ANone(Leibniz.refl[A])
+  def empty[F[_, _], A]: AOption[F, A, A] = None.asInstanceOf[AOption[F, A, A]]
+
+  private val None = none[Nothing, Nothing]
+  private def none[F[_, _], A]: AOption[F, A, A] = new ANone[F, A, A] {
+    def   subst[G[_]](ga: G[A]): G[A] = ga
+    def unsubst[G[_]](gb: G[A]): G[A] = gb
+  }
 }
